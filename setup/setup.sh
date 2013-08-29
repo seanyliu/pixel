a0fcbcee24155ef8c9e10d4c5d3daf51bcb66ef8
Ubuntu <ubuntu@ip-10-202-35-253.ec2.internal>
2013-08-29 19:37:05 +0000

    modified setup.sh to point to own git files

--- setup/setup.sh
+++ setup/setup.sh
@@ -39,7 +39,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/seanyliu/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
