90e26d2cb07892a516aa404946df81859b189561
Ubuntu <ubuntu@ip-10-202-35-253.ec2.internal>
2013-09-03 04:31:25 +0000

    added setup script

---
+++ setup/setup-ssjs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Install node packages
+npm install
+
+echo "Please enter your github id:"
+read GITHUB_USERNAME
+echo "You entered: $GITHUB_USERNAME"
+
+echo "Please enter your email (for github commits):"
+read GITHUB_EMAIL
+echo "You entered: $GITHUB_EMAIL"
+
+git config --global user.name $GITHUB_USERNAME
+git config --global user.email $GITHUB_EMAIL
+
+# Login to Heroku, then create apps.
+# See:
+# https://devcenter.heroku.com/articles/keys
+# https://devcenter.heroku/articles/multiple-environments
+heroku login
+heroku keys:add
+heroku apps:create $GITHUB_USERNAME-s-pixelalchemies --remote staging-heroku
+heroku apps:create $GITHUB_USERNAME-pixelalchemies --remote production-heroku
