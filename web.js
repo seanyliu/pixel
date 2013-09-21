62be3ea5bacb7419641dddc90e9d6f9949078fad
seanyliu <sean.y.liu@gmail.com>
2013-09-21 00:12:51 +0000

    updating index.html

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var http = require('http');
+
+/*
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 app.use("/images", express.static(__dirname + '/images'));
+app.use("/techDemo", express.static(__dirname + '/techDemo'));
 
 app.get('/', function(request, response) {
   var html = fs.readFileSync('index.html');
@@ -13,3 +17,14 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
+var app = express();
+app.configure(function(){
+    app.use(express.static(__dirname));
+});
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var port = process.env.PORT || 8080;
+console.log("Listening on " + port);
+server.listen(port);
