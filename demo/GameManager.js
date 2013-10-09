15ec0877dc834f0f60c2ab87d6cd64310ad21e2c
seanyliu <sean.y.liu@gmail.com>
2013-10-09 03:56:21 +0000

    fixed bugs with image loading....but code is somewhat ugly while i'm partway through

---
+++ demo/GameManager.js
@@ -0,0 +1,56 @@
+function GameManager(canvasHandle) {
+
+  /* Initialize Object */
+  this.targetFPS = 20;
+
+  // Get primary canvas
+  this.mainCanvas = canvasHandle;
+  this.mainContext = this.mainCanvas.getContext('2d');
+
+  // create buffer
+  this.backBufferCanvas = document.createElement('canvas');
+  this.backBufferCanvas.width = this.mainCanvas.width;
+  this.backBufferCanvas.height = this.mainCanvas.height;
+  this.backBufferContext = this.backBufferCanvas.getContext('2d');
+
+  // initialize game state
+  this.gameState = new GameState(this.mainCanvas);
+
+  this.threadHandle = null;
+}
+
+// start rendering
+GameManager.prototype.start = function() {
+  var threadHandle = setInterval(function() {
+    GB_gameManager.render();
+  }, 1000/this.targetFPS);
+  return threadHandle;
+};
+
+// pause rendering
+GameManager.prototype.stop = function(threadHandle) {
+  if (threadHandle === null) {
+  } else {
+    clearInterval(threadHandle);
+  }
+};
+
+// actually render
+GameManager.prototype.render = function() {
+  // Clear buffer
+  this.backBufferContext = this.backBufferCanvas.getContext('2d');
+  this.backBufferContext.clearRect(0, 0,
+    this.backBufferCanvas.width, this.backBufferCanvas.height
+  );
+
+  // Render to buffer
+  this.gameState.update(this.backBufferCanvas);
+
+  // Clear the main screen
+  this.mainContext.clearRect(0, 0,
+    this.backBufferCanvas.width, this.backBufferCanvas.height
+  );
+
+  // draw buffer to main screen
+  this.mainContext.drawImage(this.backBufferCanvas, 0, 0);
+};
