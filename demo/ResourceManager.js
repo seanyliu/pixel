3c1d022f5dc299d6f42d073f450028d5e6766740
Sean Liu <sean.y.liu@gmail.com>
2013-12-21 15:34:35 -0800

    Added ResourceManager class

---
+++ demo/ResourceManager.js
@@ -0,0 +1,8 @@
+/**
+ * Manages any external resources used by the game
+ */
+function ResourceManager() {
+  this.imageProperties = null;
+  this.startupResourceManager = function(images) {
+  }
+}
