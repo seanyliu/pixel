b0dc5aec92b78a2d5b59facdc4f2a245808653f8
Sean Liu <sean.y.liu@gmail.com>
2014-03-23 22:55:44 -0400

    made enemies only spawn when on screen

--- unity/Assets/Scripts/EnemyScript.cs
+++ unity/Assets/Scripts/EnemyScript.cs
@@ -7,6 +7,8 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
 
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
 
 	void Awake () {
@@ -14,17 +16,63 @@ public class EnemyScript : MonoBehaviour {
 		// Need to put an empty game object in the enemy so that's it's rotated
 		// to shoot in the right direction.
 		weapons = GetComponentsInChildren<WeaponScript>();
+
+		// Retrieve scripts to disable when not spawn
+		moveScript = GetComponent<MoveScript>();
+	}
+
+	// Disable everything (while off screen)
+	void Start() {
+		hasSpawn = false;
+		
+		// Disable everything
+		// -- collider
+		collider2D.enabled = false;
+		// -- Moving
+		moveScript.enabled = false;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = false;
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		// Auto-fire
-		foreach (WeaponScript weapon in weapons) {
-			// Auto-fire
-			if (weapon != null && weapon.CanAttack)
-			{
-				weapon.Attack(true);
+
+		// 2 - Check if the enemy has spawned.
+		if (hasSpawn == false) {
+			if (renderer.IsVisibleFrom(Camera.main)) {
+				Spawn();
 			}
+		} else {
+			// Auto-fire
+			foreach (WeaponScript weapon in weapons) {
+				// Auto-fire
+				if (weapon != null && weapon.CanAttack)
+				{
+					weapon.Attack(true);
+				}
+			}
+
+			// Out of the camera ? Destroy the game object.
+			if (renderer.IsVisibleFrom(Camera.main) == false) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	// Activate itself (called when gets on screen)
+	private void Spawn() {
+		hasSpawn = true;
+		
+		// Enable everything
+		// -- Collider
+		collider2D.enabled = true;
+		// -- Moving
+		moveScript.enabled = true;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = true;
 		}
 	}
 }
