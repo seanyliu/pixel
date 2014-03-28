f203e3af9be44d2ce78209f37321ae3da578ecea
Sean Liu <sean.y.liu@gmail.com>
2014-03-27 20:03:57 -0400

    fixed double jump on mac

--- unity2/Assets/Scripts/PlayerScript.cs
+++ unity2/Assets/Scripts/PlayerScript.cs
@@ -2,36 +2,42 @@
 using System.Collections;
 
 /// <summary>
-/// Player controller and behavior
+/// Player controller and behavior.
+/// Note that we don't let the player move left or right, we only
+/// let them jump! They are stationary and the background scrolls.
 /// </summary>
 public class PlayerScript : MonoBehaviour {
-
-	// Speed of the player
-	public Vector2 speed = new Vector2(50,50);
-
-	// Store the movement
-	private Vector2 velocity;
-
+	
 	// Jumping collision vars
-	private bool grounded = true;
 	public Transform groundCheck;
 	public float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
+	private bool grounded = true;
 
-	// Jumping vars
-	public int maxJumps = 2;
-	private int currentJumps = 0;
+	// Jump attributes
 	public float jumpForce = 1000;
 
+	// Double jump
+	public int maxJumps = 2;
+	private int currentJumps = 0;
+
 	// Jumping touch detection
-	private bool jumpPressed = false;
-	public float jumpMinCooldown = 0.01f;
-	private float jumpCooldown;
+	private bool jumpPressed;
+	//private float jumpCooldown;
+
+	// A single touch registers a .Began and .Ended
+	// a bajillion times. So we track the touchFingerId
+	// to make sure that we only process the touch once.
 	private int touchFingerId = -1;
+
+	// Because there's a mismatch between update()
+	// and FixedUpdate, sometimes FixedUpdate can be
+	// called multiple times per touch in update().
 	private int jumpId = -1;
 	private int jumpIdProcessed = -1;
 
-	// Scoring variables
+	// Scoring variables, used for detecting if
+	// a dog runs under the user (+score in that case)
 	public LayerMask allButPlayer;
 	private int lastScoredEnemyInstanceID;
 
@@ -39,12 +45,14 @@ public class PlayerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		jumpCooldown = 0f;
+		//jumpCooldown = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		/*
+		 * public float jumpMinCooldown = 0.01f;
 		// Touch registers a TON of presses, so add a min cooldown
 		if (jumpCooldown > 0) {
 			jumpCooldown -= Time.deltaTime;
@@ -62,23 +70,8 @@ public class PlayerScript : MonoBehaviour {
 		// See http://pixelnest.io/tutorials/unity-touch-controls/
 		*/
 
-		jumpPressed = false;
-		if (Input.touchCount > 0) {
-			Touch touch = Input.GetTouch (0);
-			if (touch.phase == TouchPhase.Began) {
-				if (touch.fingerId != touchFingerId) { // new touch
-					jumpPressed = true;
-					jumpId++;
-					touchFingerId = touch.fingerId;
-					Debug.Log ("TOUCH BEGIN: "+touchFingerId);
-				}
-			} else if (touch.phase == TouchPhase.Ended) {
-				Debug.Log ("TOUCH END: "+touchFingerId);
-				touchFingerId = -1;
-			}
-		}
-
-		//Debug.Log (jumpPressed);
+		// If you put key detection in FixedUpdate, it appears sometimes it gets missed
+		jumpPressed = PressedJump();
 
 		// Make sure we are not outside the camera bounds
 		var dist = (transform.position - Camera.main.transform.position).z;
@@ -110,43 +103,49 @@ public class PlayerScript : MonoBehaviour {
 	// You should use this method over Update() when dealing
 	// with physics ("RigidBody" and forces).
 	void FixedUpdate() {
-		//Debug.Log ("velocity2: "+rigidbody2D.velocity.y);
-		// move the game object
-		// This will tell the physic engine to move the game
-		// object. We do that in FixedUpdate() as it is
-		// recommended to do everything that is physics-related in there.
 
-		// DISABLE LEFT AND RIGHT MOVEMENT
-		// rigidbody2D.velocity = movement;
-
+		// Detect if the user is on the ground
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		// Clear the number of jumps the user has made
 		if (grounded) {
-			currentJumps = 0; // allow double jumps
+			currentJumps = 0;
+			//Debug.Log ("clear grounded");
 		}
-		//Debug.Log(currentJumps);
-		
-		// JUMPING 
-		//Debug.Log ("jumpPressed: " + jumpPressed);
-		Debug.Log ("currentJumps: " + currentJumps + "; maxJumps:" + maxJumps + "; grounded: " +grounded + "; jumpId: "+jumpId+"; jumpIdProcessed:"+jumpIdProcessed+"; "+ (jumpPressed && ((currentJumps < maxJumps) || grounded) && jumpIdProcessed != jumpId));
-		if (jumpPressed && ((currentJumps < maxJumps) || grounded) && jumpIdProcessed != jumpId) {
-			Debug.Log ("JUMP1!");
+
+		// TODO: BUG: Triple jumps are allowed? Why is it that currentJumps keeps staying at 0 instead of being incremented? It's allowing triple jumps to actually happen.
+
+		/*
+currentJumps: 0; maxJumps:2; grounded: True; jumpId: 4; jumpIdProcessed:2; True
+UnityEngine.Debug:Log(Object)
+PlayerScript:FixedUpdate() (at Assets/Scripts/PlayerScript.cs:118)
+
+currentJumps: 0; maxJumps:2; grounded: False; jumpId: 5; jumpIdProcessed:4; True
+UnityEngine.Debug:Log(Object)
+PlayerScript:FixedUpdate() (at Assets/Scripts/PlayerScript.cs:118)
+
+currentJumps: 1; maxJumps:2; grounded: False; jumpId: 7; jumpIdProcessed:5; True
+UnityEngine.Debug:Log(Object)
+PlayerScript:FixedUpdate() (at Assets/Scripts/PlayerScript.cs:118)
+
+*/
+		if (jumpPressed) {
+			Debug.Log ("currentJumps: " + currentJumps + "; maxJumps:" + maxJumps + "; grounded: " +grounded + "; jumpId: "+jumpId+"; jumpIdProcessed:"+jumpIdProcessed+"; "+ (jumpPressed && ((currentJumps < maxJumps) || grounded) && jumpIdProcessed != jumpId));
+		}
+		// Jumping
+		// Sometimes jumpPressed is off at this point... seems to fix it. But don't want to process a queue of jumps, so clear the queue.
+		if (((currentJumps < maxJumps) || grounded) && jumpIdProcessed != jumpId) {
+			Debug.Log ("JUMP!");
+			Debug.Log ("currentJumps: " + currentJumps + "; maxJumps:" + maxJumps + "; grounded: " +grounded + "; jumpId: "+jumpId+"; jumpIdProcessed:"+jumpIdProcessed+"; "+ (jumpPressed && ((currentJumps < maxJumps) || grounded) && jumpIdProcessed != jumpId));
 			rigidbody2D.velocity = new Vector2(0, 0);
 			rigidbody2D.AddForce(new Vector2(0, jumpForce));
 			currentJumps++;
 			jumpIdProcessed = jumpId;
+		} else if (currentJumps >= maxJumps) {
+			jumpIdProcessed = jumpId;
 		}
 
-		/*
-
-		if ((grounded || (currentJumps < maxJumps - 1)) && jumpPressed) {
-			Debug.Log ("JUMP2!");
-			rigidbody2D.velocity = new Vector2(0, 0);
-			rigidbody2D.AddForce(new Vector2(0, jumpForce));
-			currentJumps++;
-		}
-		*/
-
-		// SCORING
+		// Scoring
 		RaycastHit2D hit = Physics2D.Raycast (transform.position, -Vector2.up, Mathf.Infinity, allButPlayer);
 		if (hit != null) {
 			//Debug.Log(hit.collider.gameObject.name);
@@ -185,5 +184,37 @@ public class PlayerScript : MonoBehaviour {
 		// Add the script to the parent because the current game
 		// object is likely going to be destroyed immediately.
 		transform.parent.gameObject.AddComponent<GameOverScript>();
+	}
+	
+
+	/// <summary>
+	/// Detect if a "Jump" key was pressed
+	/// </summary>
+	/// <returns><c>true</c>, if jump was pressed, <c>false</c> otherwise.</returns>
+	bool PressedJump() {
+
+		// First, detect for mobile devices
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch (0);
+			if (touch.phase == TouchPhase.Began) {
+				if (touch.fingerId != touchFingerId) { // new touch
+					jumpId++;
+					touchFingerId = touch.fingerId;
+					Debug.Log ("Touch begin: "+touchFingerId);
+					return true;
+				}
+			} else if (touch.phase == TouchPhase.Ended) {
+				Debug.Log ("Touch end: "+touchFingerId);
+				touchFingerId = -1;
+			}
+		}
+
+		else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			Debug.Log ("Up key press");
+			jumpId++;
+			return true;
+		}
+
+		return false;
 	}
 }
